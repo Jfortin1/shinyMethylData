9fa667f1d96034824ae167c743d2b75cb893db84
jfortin <jfortin@jhsph.edu>
2016-05-20 09:17:45 -0400

    Cleaned code

--- inst/scripts/gen.summary.tcga.raw.R
+++ inst/scripts/gen.summary.tcga.raw.R
@@ -1,14 +1,12 @@
 library(shinyMethyl)
-dataDir <- "/dcs01/oncbio/ejfertig/HNSCC450K"
+extdir <- "/dcs01/hansen/hansen_lab1/shinyMethylDatasets"
+load(file.path(extdir,"RGSetHNSC.Rda"))
+load(file.path(extdir,"cov.tcga.Rda"))
+RGSet <- updateObject(RGSet)
 
-
-# RGSetHNSC is an RGChannelSet containing the 369 samples from TCGA HNSC
-setwd(dataDir)
-load("RGSetHNSC.Rda")
-load("cov.tcga.Rda")
 summary.tcga.raw <- shinySummarize(RGSet)
 summary.tcga.raw@phenotype <- cov.tcga
+save(summary.tcga.raw, file = "../../data/summary.tcga.raw.rda")
 
-save(summary.tcga.raw, file = "summary.tcga.raw.rda")
 
 
